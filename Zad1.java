ae0dc367d16d317ae4756a2f57b988fe65766272
sleepyeldrazi <konik98@gmail.com>
2017-01-16 22:21:53 +0200

    zad 1 commented

--- Zad1.java
+++ Zad1.java
@@ -1,9 +1,6 @@
 /**
  * Created by Konik on 1/16/17.
  */
-/**
- * Created by Konik on 1/16/17.
- */
 import java.awt.*;
 
 import javax.swing.JFrame;
@@ -12,31 +9,27 @@ import java.awt.event.*;
 
 public class Zad1 extends Canvas implements MouseListener,
         MouseMotionListener {
-    int x,y;
-    int r=0;
-    Point p;
+    int r=0; //radius
+    Point p; // polzvam p za segashnata poziciq na mishkata (po kratko otkolkoto oshte 2 promenlivi xy)
 
-    // boolean clear=false;
-
-    public Zad1() {
+    public Zad1() { // konstruktora si e qsen smsl trqbva da se add-nat 2ta listener-i
         addMouseListener(this);
         addMouseMotionListener(this);
-
     }
 
     public void paint(Graphics g) {
-        g.setColor(Color.red);
-        g.fillOval(x-3,y-3,6,6);
+        g.setColor(Color.red); // i like red
+        g.fillOval(p.x-3,p.y-3,6,6); // pravi "to4kata" kadeto si natisnal kato e s radius 3
         g.drawOval(p.x-r, p.y-r, 2*r, 2*r);
-
+        //4ertae ot p(mishka) - radius  do 2* radiusa t.e. diametar zashtoto trqbva ppc da se vavejda
+        // "shiro4inata" i "viso4inata" na kraga (diametri = 2*r)
+        //poneje mishkata vinagi e na r raztoqnie ot centara zatova se vadi r ot x i y na mishkata
     }
 
     public void mousePressed(MouseEvent evt) {
-       p= evt.getPoint();
-       x=p.x;
-       y=p.y;
-       r=0;
-       repaint();
+       p= evt.getPoint(); // p= lokaciqta na mishkata
+       r=0; // na4alen radius
+       repaint(); // risuvame
 
     }
 
@@ -53,7 +46,8 @@ public class Zad1 extends Canvas implements MouseListener,
     }
 
     public void mouseDragged(MouseEvent evt) {
-        r = (int) Math.round(evt.getPoint().distance(p));
+        r = (int) Math.round(evt.getPoint().distance(p)); // radiusa e raven na raztoqnieto ot tam kadeto e bilo
+        // natisnato(p) do segashnoto mqsto (evt.getPoint())
         repaint();
 
         repaint();
@@ -64,10 +58,10 @@ public class Zad1 extends Canvas implements MouseListener,
 
     //public void update(Graphics g){paint(g);}
     public static void main(String[] args) {
-        JFrame win = new JFrame("Sexy Gabi");
-        win.setSize(1024, 768);
-        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        win.add(new Zad1());
-        win.setVisible(true);
+        JFrame win = new JFrame("Sexy Gabi"); // setup na frame
+        win.setSize(1024, 768);//size na frame
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // bullshit
+        win.add(new Zad1()); // dobavqne na canvas-a v frame-a
+        win.setVisible(true); // let it shine
     }
 }
