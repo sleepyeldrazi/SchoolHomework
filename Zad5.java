6575426a7bd2e49c4e842b79dd424f87841d73bb
sleepyeldrazi <konik98@gmail.com>
2017-01-16 21:23:12 +0200

    Zad 5 done

--- Zad5.java
+++ Zad5.java
@@ -9,7 +9,10 @@ import java.awt.event.MouseMotionListener;
  */
 public class Zad5 extends Canvas implements MouseListener,
         MouseMotionListener {
-
+    Color colorSq = Color.blue;
+    Color colorBg = Color.yellow;
+    int x= -30;
+    int y= -30;
 
 
 
@@ -20,16 +23,31 @@ public class Zad5 extends Canvas implements MouseListener,
     }
 
     public void paint(Graphics g) {
-
+        g.setColor(colorSq);
+        setBackground(colorBg);
+        g.fillRect(x - 20, y - 20, 40, 40);
 
     }
 
     public void mousePressed(MouseEvent evt) {
-
+        x = evt.getX();
+        y = evt.getY();
     }
 
     public void mouseClicked(MouseEvent evt) {
-
+        if(colorSq == Color.blue){
+            colorSq = Color.yellow;
+            colorBg = Color.red;
+        }
+        else if(colorSq == Color.yellow) {
+            colorSq = Color.red;
+            colorBg = Color.blue;
+        }
+        else {
+            colorSq = Color.blue;
+            colorBg = Color.yellow;
+        }
+        repaint();
     }
 
     public void mouseReleased(MouseEvent evt) {
@@ -42,7 +60,9 @@ public class Zad5 extends Canvas implements MouseListener,
     }
 
     public void mouseDragged(MouseEvent evt) {
-
+        x = evt.getX();
+        y = evt.getY();
+        repaint();
     }
 
     public void mouseMoved(MouseEvent evt) {
--- Zad6.java
+++ Zad6.java
@@ -1,49 +1,41 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Konik on 1/16/17.
  */
-public class Zad3 extends Canvas implements MouseListener,
+public class Zad6 extends Canvas implements MouseListener,
         MouseMotionListener {
-    Color colorSq = Color.blue;
-    Color colorBg = Color.yellow;
-    int x= -30;
-    int y= -30;
+
+    BufferedImage rock = ImageIO.read(new File("rock.png"));
+    BufferedImage paper = ImageIO.read(new File("paper.jpg"));
+    BufferedImage scisscors = ImageIO.read(new File("scissors.jpg"));
 
 
-
-    public Zad3() {
+    public Zad6() throws IOException {
         addMouseListener(this);
         addMouseMotionListener(this);
 
     }
 
     public void paint(Graphics g) {
-        g.setColor(colorSq);
-        setBackground(colorBg);
-        g.fillRect(x - 20, y - 20, 40, 40);
+    g.drawImage(rock, 20, 20, 320, 320, null, null);
 
     }
 
     public void mousePressed(MouseEvent evt) {
-    x = evt.getX();
-    y = evt.getY();
+
     }
 
     public void mouseClicked(MouseEvent evt) {
-        if(colorSq == Color.blue){
-        colorSq = Color.yellow;
-        colorBg = Color.blue;
-        }
-        else {
-            colorSq = Color.blue;
-            colorBg = Color.yellow;
-        }
-        repaint();
+
     }
 
     public void mouseReleased(MouseEvent evt) {
@@ -56,20 +48,18 @@ public class Zad3 extends Canvas implements MouseListener,
     }
 
     public void mouseDragged(MouseEvent evt) {
-        x = evt.getX();
-        y = evt.getY();
-        repaint();
+
     }
 
     public void mouseMoved(MouseEvent evt) {
     }
 
     //public void update(Graphics g){paint(g);}
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         JFrame win = new JFrame("Sexy Sky");
         win.setSize(1024, 768);
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        win.add(new Zad3());
+        win.add(new Zad6());
         win.setVisible(true);
     }
 }
